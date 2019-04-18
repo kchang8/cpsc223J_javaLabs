fb29e8007a39ed437d7cb9ad75cb2ecf5df5ce9c
kchang8 <36432965+kchang8@users.noreply.github.com>
2019-04-17 17:31:08 -0700

    Add files via upload

---
+++ Lab_09/lab9.java
@@ -0,0 +1,13 @@
+//Kailie Chang
+//CPSC 223J TuTh 8:00AM
+//Lab 9: Temperature Converter
+//creates an application using JFrame and ActionListener
+
+public class lab9 {
+
+	public static void main(String[] args) {
+		TempConverter frame = new TempConverter();
+		frame.setVisible(true);
+	}
+
+}
