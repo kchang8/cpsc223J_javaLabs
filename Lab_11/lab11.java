79f058c45183aecc3a1b668aebcd39dad1c66198
kchang8 <36432965+kchang8@users.noreply.github.com>
2019-04-17 17:34:08 -0700

    Add files via upload

---
+++ Lab_11/lab11.java
@@ -0,0 +1,15 @@
+//Kailie Chang
+//CPSC 223J TuTh 8:00AM
+//Lab 11
+//writes a quiz application that asks the user a set of 4 questions
+//utilizing JCheckBox and JComboBox
+
+public class lab11 {
+
+	public static void main(String[] args) {
+		QuizApp myQuiz = new QuizApp();
+		myQuiz.setVisible(true);
+
+	}
+
+}
