fdab35b86513c530c7d704a170ba38a625dde698
kchang8 <36432965+kchang8@users.noreply.github.com>
2019-04-17 17:35:18 -0700

    Create lab12.java

---
+++ Lab_12/lab12.java
@@ -0,0 +1,14 @@
+//Kailie Chang
+//CPSC 223J TuTh 8:30AM
+//Lab 12
+//makes a dice game using JFrame
+
+public class lab12 {
+
+	public static void main(String[] args) {
+		DiceGame myDice = new DiceGame();
+		myDice.setVisible(true);
+
+	}
+
+}
