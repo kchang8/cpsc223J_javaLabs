6a922c97c16bfec733ea8d000a129ad11f316573
kchang8 <36432965+kchang8@users.noreply.github.com>
2019-04-17 17:36:21 -0700

    Create lab13.java

---
+++ Lab_13/lab13.java
@@ -0,0 +1,14 @@
+//Kailie Chang
+//CPSC 223J TuTh 8:00AM
+//Lab 13
+//JFrame Mad Libs: randomly select one of two mad libs to
+//display user when the next card is displayed
+
+public class lab13 {
+
+	public static void main(String[] args) {
+		JCardLayout myCard = new JCardLayout();
+		myCard.setVisible(true);
+	}
+
+}
