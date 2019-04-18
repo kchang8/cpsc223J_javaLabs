1c05796dfe49bb9b9066802e9393292a30cadfce
kchang8 <36432965+kchang8@users.noreply.github.com>
2019-04-17 17:32:41 -0700

    Add files via upload

---
+++ Lab_10/lab10.java
@@ -0,0 +1,14 @@
+//Kailie Chang
+//CPSC 223J-01
+//Lab 10
+//Write a program that stimulates a calculator
+
+public class lab10 {
+
+	public static void main(String[] args) {
+		Calculator myCalc = new Calculator();
+		myCalc.setVisible(true);
+
+	}
+
+}
